1b824c59157f7f7469a501dd0d9206e414908bff
Noah <noah.p.kleineweischede@gmail.com>
2025-09-01 13:35:40 +0200

    feat: Bullets will be removed after leaving the camera view

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -3,10 +3,30 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+    public new Camera camera;
+    public new Renderer renderer;
     public Rigidbody2D rb;
     public float speed;
+
+    private void Start()
+    {
+        if (camera == null) camera = Camera.main;
+        renderer = GetComponent<Renderer>();
+    }
+    
     private void FixedUpdate()
     {
         rb.linearVelocity = new Vector2(rb.linearVelocity.x, speed);
+    }
+
+    private void Update()
+    {
+        if (!IsInView()) Destroy(gameObject);
+    }
+
+    private bool IsInView()
+    {
+        var planes = GeometryUtility.CalculateFrustumPlanes(camera);
+        return GeometryUtility.TestPlanesAABB(planes, renderer.bounds);
     }
 }
